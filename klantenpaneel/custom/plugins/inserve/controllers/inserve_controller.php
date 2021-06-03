035bf5e01c219549476d8168d1f63de6180cab77
Marnick Menting <marnick@innovaat.nl>
2021-06-03 15:20:31 +0200

    login working

--- klantenpaneel/custom/plugins/inserve/controllers/inserve_controller.php
+++ klantenpaneel/custom/plugins/inserve/controllers/inserve_controller.php
@@ -3,8 +3,7 @@
 namespace Inserve;
 
 use Template;
-use Plugin;
-use Settings_Model;
+use Debtor_Model;
 
 class Inserve_Controller extends \Base_Controller
 {
@@ -16,6 +15,19 @@ class Inserve_Controller extends \Base_Controller
 
 	public function index()
 	{
+        $apiKey = "xz8l9Yk4V2ZUOxZ8Rh3h6dVecAZJmbkSCNbUufy4c58qhf48qJCgXSjpMNEEgdTJ";
+        $baseUrl = 'http://test.inserve-api.loc';
+	    $api = new Inserve_Model($baseUrl, $apiKey);
+        $debtor = new Debtor_Model();
+        $debtor->show();
+        $debtorCode = $debtor->DebtorCode;
+        $companies = $api->get('/companies?builder[0][where][0]=debtor_code&builder[0][where][1]=' . $debtorCode);
+        $companyId = $companies[0]['id'];
+        $clients = $api->get('/clients?builder[0][where][0]=company_id&builder[0][where][1]=' . $companyId);
+        $email = $clients[0]['email'];
+        $token = $api->post('/auth/access-token', ['username' => $email]);
+        $this->Template->accessToken = $token['access_token'];
+
         $this->Template->show('inserve');
 	}
 
